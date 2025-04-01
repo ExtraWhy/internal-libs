80126911c63e25e1df23c33c6e8405462ac86678
carnophager <carnophager@gmail.com>
2025-04-01 17:46:40 +0300

    feat: added email field to users

--- models/user/user.go
+++ models/user/user.go
@@ -2,6 +2,7 @@ package user
 
 type User struct {
 	Id       uint64 `json:"id"`
+	Token    string `json:"token"`
 	Username string `json:"username"`
 	Email    string `json:"email"`
 	Picture  string `json:"picture"`
