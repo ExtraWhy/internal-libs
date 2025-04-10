fd82b6c1881ddf601e95857148267d9bf6822f8a
ilian <heatblazer@gmail.com>
2025-04-11 02:13:04 +0300

    default players array

--- db/players.go
+++ db/players.go
@@ -93,7 +93,7 @@ func (db *NoSqlConnection) DisplayPlayers() []player.Player {
 	if err != nil {
 		return nil
 	}
-	var players = make([]player.Player, 5)
+	var players []player.Player
 	for cursor.Next(context.TODO()) {
 		var elem player.Player
 		err := cursor.Decode(&elem)
