e06c6953c1584295cd049b4543375c494e6ea907
ilian <heatblazer@gmail.com>
2025-04-10 23:35:39 +0300

    added db interface calls

--- db/db-interface.go
+++ db/db-interface.go
@@ -1,10 +1,18 @@
 package db
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/ExtraWhy/internal-libs/models/player"
+)
 
 type DbIface interface {
 	Init(driver string, dsn string) error
 	Deinit() error
+	UpdatePlayerMoney(p *player.Player) (int64, error)
+	DisplayPlayers() []player.Player
+	AddPlayer(p *player.Player) bool
+	CreatePlayersTable() error
 }
 
 type UnimplementedDbConnector struct {
@@ -12,14 +20,26 @@ type UnimplementedDbConnector struct {
 
 func (UnimplementedDbConnector) mustEmbedUnimplementedDbConnector() {}
 
+func (UnimplementedDbConnector) CreatePlayersTable() error {
+	return fmt.Errorf("Must implement method Init")
+}
+
+func (UnimplementedDbConnector) AddPlayer(p *player.Player) bool {
+	return false
+}
+
+func (UnimplementedDbConnector) UpdatePlayerMoney(p *player.Player) (int64, error) {
+	return -1, fmt.Errorf("Must implement method Init")
+}
+
+func (UnimplementedDbConnector) DisplayPlayers() []player.Player {
+	return nil
+}
+
 func (UnimplementedDbConnector) Init(driver string, dsn string) error {
 	return fmt.Errorf("Must implement method Init")
 }
 
 func (UnimplementedDbConnector) Deinit() error {
-	return fmt.Errorf("Must implement method Deinit")
-}
-
-func (UnimplementedDbConnector) Connect() error {
 	return fmt.Errorf("Must implement method Deinit")
 }
