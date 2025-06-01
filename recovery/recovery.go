ca948733df85eb3a5c5bfa243aa1164492dd1603
ilian <heatblazer@gmail.com>
2025-06-01 11:00:21 +0300

    wip/user and recovery

--- recovery/recovery.go
+++ recovery/recovery.go
@@ -2,26 +2,7 @@ package recovery
 
 import (
 	"github.com/ExtraWhy/internal-libs/models/games"
+	"github.com/ExtraWhy/internal-libs/models/user"
 )
 
-var sGames = games.Games{}
-
-//ID          uint32 `json:"id"`
-//	FreeGames   uint32 `json:"freegames"`
-//	TotalPlayed uint64 `json:"num_played"`
-//	TotalWins   uint64 `json:"total_wins"`
-
-func AddToRecovery(gameid, freegames uint32, totalplayed, wins uint64) {
-	gd := games.GameData{ID: gameid, FreeGames: freegames, TotalPlayed: totalplayed, TotalWins: wins}
-	sGames.GameList = append(sGames.GameList, gd)
-	sGames.LastPlayedGame = gameid
-}
-
-func GetGameById(gameid uint32) *games.GameData {
-	for i := 0; i < len(sGames.GameList); i++ {
-		if sGames.GameList[i].ID == gameid {
-			return &sGames.GameList[i]
-		}
-	}
-	return nil
-}
+var sGames = map[user.User]games.Games{}
