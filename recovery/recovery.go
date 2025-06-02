c15f36e2a0fab0b6f1e05da0b06e1e5b3c682af9
ilian <heatblazer@gmail.com>
2025-06-03 00:22:50 +0300

    recovery wip

--- recovery/recovery.go
+++ recovery/recovery.go
@@ -1,8 +1,33 @@
 package recovery
 
 import (
+	"fmt"
+
 	"github.com/ExtraWhy/internal-libs/models/games"
-	"github.com/ExtraWhy/internal-libs/models/user"
 )
 
-var sGames = map[user.User]games.Games{}
+type MapUserGames map[string]games.Games
+
+var sGames = MapUserGames{}
+
+func AddRecovery(usr string, gameid, free uint32, won uint64) {
+	if v, ok := sGames[usr]; ok {
+		for i := 0; i < len(v.GameList); i++ {
+			if v.GameList[i].ID == gameid {
+				v.LastPlayedGame = gameid
+				if free > 0 {
+					v.GameList[i].FreeGames += free
+				}
+				if won > 0 {
+					v.GameList[i].TotalWins++
+				}
+				v.GameList[i].TotalPlayed++
+			}
+		}
+	}
+}
+
+// test only for now !!!
+func DumpRecovery() {
+	fmt.Println(sGames)
+}
