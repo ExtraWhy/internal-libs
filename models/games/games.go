ab7e9189fb131ed3e5d7d5444c139d1075257c29
ilian <heatblazer@gmail.com>
2025-06-20 10:34:52 +0300

    wip games

--- models/games/games.go
+++ models/games/games.go
@@ -3,11 +3,13 @@ package games
 // game pair will hold all relevant data needed for the games
 // for recovery and user state like how much games are
 // also we can have the rtp as an info and a hash code for the random number (TBD)
+// reserved for casinobet and future uses
 type GameData struct {
 	ID          uint32 `json:"id"`
 	FreeGames   uint32 `json:"freegames"`
 	TotalPlayed uint64 `json:"num_played"`
 	TotalWins   uint64 `json:"total_wins"`
+	Reserved    any    `json:"reserved"`
 }
 
 type Games struct {
