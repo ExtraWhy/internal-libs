a58f386b8abb5ea9e9ab3fa4b8cd13167f86ff97
heatblazer <heatblazer@gmail.com>
2025-07-14 04:33:15 +0300

    wip barfill

--- models/player/player.go
+++ models/player/player.go
@@ -6,14 +6,16 @@ type SpecializedID interface {
 	uint64 | uuid.UUID
 }
 
+type CB_Barfill [6]int
+
 // player structure for playing a game
 type Player[T SpecializedID] struct {
 	Id    T      `json:"id"`
 	Money uint64 `json:"money"`
 	//related for main/casinobet/dev
-	DailyLimit      uint64 `json:"daily_limit"`
-	TotalWonDaily   uint64 `json:"total_won_daily"`
-	PointsForReward uint64 `json:"points_for_reward"`
-	Name            string `json:"name"`
-	BarFill         string `json:"bar_fill"`
+	DailyLimit      uint64     `json:"daily_limit"`
+	TotalWonDaily   uint64     `json:"total_won_daily"`
+	PointsForReward uint64     `json:"points_for_reward"`
+	Name            string     `json:"name"`
+	BarFill         CB_Barfill `json:"bar_fill"`
 }
